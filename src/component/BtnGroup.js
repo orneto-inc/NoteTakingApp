8c4c32df6007ff1370e0dfe4de4f504c069b47ab
avinashdukaan <130451285+avinashdukaan@users.noreply.github.com>
2023-10-10 15:37:19 +0530

    note taking first commit

---
+++ src/component/BtnGroup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+const BtnGroup = () =>{
+    return(
+        <div className='btn-grp'>
+            <button className='btn btn-primary'>Create Note</button>
+        </div>
+    )
+}
+export default BtnGroup;
---
+++ src/component/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+import BtnGroup from './BtnGroup'
+
+const HomePage = () =>{
+    return(
+        <div className='homepage'>
+            <BtnGroup />
+        </div>
+    )
+}
+export default HomePage;
