d9e606d6b1870c52b8cab920f5a1aab7f55032a4
orneto-incm <orneto11@gmail.com>
2023-11-07 14:30:07 +0530

    assedts added, login page

---
+++ client/src/component/LoginPage/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../assets/styles.css";
+
+
+const LoginPage = () => {
+  return (
+    <div className="loginpage-container">
+      
+    </div>
+  );
+};
+export default LoginPage;
